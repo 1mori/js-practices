4ac2a407726a5f9f8e7545a7f23538c05f1db377
yuichi.imori <yuichi.imori@classi.jp>
2024-05-29 18:43:46 +0900

    今日の日付と最終日の日付を取得し、カレンダーとして出力する

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -15,3 +15,29 @@ if (argv["y"] !== undefined) {
 if (argv["m"] !== undefined) {
   month = Number(argv["m"]);
 }
+
+const first_date = new Date(year, month, 1);
+const last_date = new Date(year, month + 1, 0);
+
+console.log("      " + month + "月 " + year);
+console.log("日 月 火 水 木 金 土");
+
+const spaces = " ".repeat(3);
+
+for (let i = 0; i < first_date.getDay(); i++) {
+  process.stdout.write(spaces);
+}
+
+for (
+  let current_date = first_date;
+  current_date <= last_date;
+  current_date.setDate(current_date.getDate() + 1)
+) {
+  process.stdout.write(
+    current_date.getDate().toString().padStart(2, " ") + " ",
+  );
+  if (current_date.getDay() === 6) {
+    console.log();
+  }
+}
+console.log();
