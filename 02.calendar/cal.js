b60d8a53536b8dca02aeb86d74205ffa7bca1e11
yuichi.imori <yuichi.imori@classi.jp>
2024-05-29 18:42:29 +0900

    コマンドラインで取得した年と月をyearとmonthに代入

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
+import minimist from "minimist";
+
+var argv = minimist(process.argv.slice(2));
+
 const today = new Date();
 let year = today.getFullYear();
 let month = today.getMonth() + 1;
+
+if (argv["y"] !== undefined) {
+  year = Number(argv["y"]);
+}
+
+if (argv["m"] !== undefined) {
+  month = Number(argv["m"]);
+}
