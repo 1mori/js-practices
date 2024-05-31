f420514674a8cdefb76d87caae15816b0503c834
yuichi.imori <yuichi.imori@classi.jp>
2024-05-31 18:00:11 +0900

    標準入力を受け付ける処理を追加

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,2 +1,22 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+import minimist from "minimist";
 import sqlite3 from "sqlite3";
 
+const argv = minimist(process.argv.slice(2));
+
+const rl = readline.createInterface({ input, output });
+
+async function main() {
+  try {
+    const answer = await rl.question("");
+    // SQLの挿入処理を書く
+  } catch (err) {
+    console.error("Error reading input:", err);
+  } finally {
+    rl.close();
+  }
+}
+
+main();
