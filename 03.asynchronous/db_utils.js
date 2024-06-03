0f7e410f7ae8c95c78e8705b836d412a9d9cc235
yuichi.imori <yuichi.imori@classi.jp>
2024-06-03 15:51:25 +0900

    closePromiseを定義

--- 03.asynchronous/db_utils.js
+++ 03.asynchronous/db_utils.js
@@ -21,3 +21,15 @@ export function allPromise(db, query) {
     });
   });
 }
+
+export function closePromise(db) {
+  return new Promise((resolve, reject) => {
+    db.close((err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
--- 03.asynchronous/promise_error.js
+++ 03.asynchronous/promise_error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runPromise, allPromise } from "./db_utils.js";
+import { runPromise, allPromise, closePromise } from "./db_utils.js";
 
 const db = new sqlite3.Database(":memory:");
 
