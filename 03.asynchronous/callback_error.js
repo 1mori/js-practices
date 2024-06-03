62acba13485f6dfe0d1ca035a046340e2f7c83da
yuichi.imori <yuichi.imori@classi.jp>
2024-06-03 15:38:17 +0900

    serializeを使わない処理に変更

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -2,46 +2,29 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.serialize(() => {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    (err) => {
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    console.log("Create table");
+
+    // 非NULLなカラムなので、NULLを挿入してみようとしてみる
+    db.run("INSERT INTO books (title) VALUES (NULL)", function (err) {
       if (err) {
-        console.error("Table create error", err);
-        return;
+        console.error("Insert error", err);
       }
-      console.log("Table created");
 
-      db.run("INSERT INTO books (title) VALUES ('あいうえお')", function (err) {
+      // 存在しないテーブルの名前を指定してみる
+      db.all("SELECT * FROM names", (err) => {
         if (err) {
-          console.error("Insert error", err);
-          return;
+          console.error("Select error", err);
         }
-        console.log("Insert book title with ID", this.lastID);
 
-        // 非NULLなカラムなので、NULLを挿入してみようとしてみる
-        db.run("INSERT INTO books (title) VALUES (NULL)", function (err) {
-          if (err) {
-            console.error("Insert error", err);
-          }
+        db.run("DROP TABLE books", () => {
+          console.log("Table dropped");
 
-          // 存在しないテーブルの名前を指定してみる
-          db.all("SELECT * FROM names", (err) => {
-            if (err) {
-              console.error("Select error", err);
-            }
-
-            db.run("DROP TABLE books", (err) => {
-              if (err) {
-                console.log("Drop error", err);
-                return;
-              }
-              console.log("Table dropped");
-              db.close();
-            });
-          });
+          db.close();
         });
       });
-    },
-  );
-});
+    });
+  },
+);
