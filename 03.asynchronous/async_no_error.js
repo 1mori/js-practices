7a04d35e22fe3cc5a70f6f77e285fe766490ea1f
yuichi.imori <yuichi.imori@classi.jp>
2024-06-03 16:14:49 +0900

    Top-Level Awaitを用いた書き方に変更、try catchを削除

--- 03.asynchronous/async_no_error.js
+++ 03.asynchronous/async_no_error.js
@@ -3,31 +3,23 @@ import { runPromise, allPromise, closePromise } from "./db_utils.js";
 
 const db = new sqlite3.Database(":memory:");
 
-const asynchronous = async () => {
-  try {
-    await runPromise(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("Create table");
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+console.log("Create table");
 
-    const result = await runPromise(
-      db,
-      "INSERT INTO books (title) VALUES ('Git入門')",
-    );
-    console.log("Insert book title with ID", result.lastID);
+const result = await runPromise(
+  db,
+  "INSERT INTO books (title) VALUES ('Git入門')",
+);
+console.log("Insert book title with ID", result.lastID);
 
-    const rows = await allPromise(db, "SELECT * FROM books");
-    console.log("Rows: ", rows);
+const rows = await allPromise(db, "SELECT * FROM books");
+console.log("Rows: ", rows);
 
-    await runPromise(db, "DROP TABLE books");
-    console.log("Table dropped");
-  } catch (err) {
-    console.error("Error: ", err);
-  } finally {
-    await closePromise(db);
-    console.log("Closed database");
-  }
-};
+await runPromise(db, "DROP TABLE books");
+console.log("Table dropped");
 
-asynchronous();
+await closePromise(db);
+console.log("Closed database");
