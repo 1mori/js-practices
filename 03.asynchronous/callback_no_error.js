0b589120e375e75c4361402ac8bb3b6b7dccc6c0
yuichi.imori <yuichi.imori@classi.jp>
2024-06-03 15:25:14 +0900

    エラーなしプログラムのエラー処理を削除

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -2,24 +2,22 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.serialize(() => {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    (err) => {
-      console.log("Table created");
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    console.log("Create table");
 
-      db.run("INSERT INTO books (title) VALUES ('あいうえお')", function (err) {
-        console.log("Insert book title with ID", this.lastID);
+    db.run("INSERT INTO books (title) VALUES ('Git入門')", function () {
+      console.log("Insert book title with ID", this.lastID);
 
-        db.all("SELECT * FROM books", (err, rows) => {
-          console.log("Rows :", rows);
+      db.all("SELECT * FROM books", (_, rows) => {
+        console.log("Rows :", rows);
 
-          db.run("DROP TABLE books", (err) => {
-            console.log("Table dropped");
-            db.close();
-          });
+        db.run("DROP TABLE books", () => {
+          console.log("Table dropped");
+          db.close();
         });
       });
-    },
-  );
-});
+    });
+  },
+);
