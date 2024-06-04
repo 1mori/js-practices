fa8eaf67500492a70f2f7f07afb4de030cb2f6ff
yuichi.imori <yuichi.imori@classi.jp>
2024-06-04 12:43:48 +0900

    mode: 非同期処理で必要なrunPromise, allPromise, closePromiseをdb_utils.jsに移動

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -6,47 +6,13 @@ import { stdin as input, stdout as output } from "node:process";
 import minimist from "minimist";
 import sqlite3 from "sqlite3";
 
+import { runPromise, allPromise, closePromise } from "./db_utils.js";
+
 const argv = minimist(process.argv.slice(2));
 
 const rl = readline.createInterface({ input, output });
 
 const db = new sqlite3.Database("./memo.sqlite3");
-
-function runPromise(db, query, params = []) {
-  return new Promise((resolve, reject) => {
-    db.run(query, params, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(this);
-      }
-    });
-  });
-}
-
-function allPromise(db, query, params = []) {
-  return new Promise((resolve, reject) => {
-    db.all(query, params, (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  });
-}
-
-function closePromise(db) {
-  return new Promise((resolve, reject) => {
-    db.close((err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
 
 var inputText = "";
 
