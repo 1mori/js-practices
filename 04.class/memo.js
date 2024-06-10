95ae247ef0c004866bfa2b046d4920412ff9566e
yuichi.imori <yuichi.imori@classi.jp>
2024-06-10 19:11:41 +0900

    change: メモのidは必要ないので、取得しないように変更

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -23,7 +23,7 @@ async function closeDatabase(db) {
 async function getMemoRows(db) {
   let memoRows;
   try {
-    memoRows = await allPromise(db, "SELECT * FROM memo");
+    memoRows = await allPromise(db, "SELECT text FROM memo");
   } catch (err) {
     if (err instanceof Error && err.code === "SQLITE_ERROR")
       console.error(err.message);
