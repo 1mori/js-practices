1664f995d870ab1119e3888628b9abca178c141f
yuichi.imori <yuichi.imori@classi.jp>
2024-06-18 15:26:01 +0900

    change: `db_utils.js`の処理を`memo-database.js`に組み込み削除
    db_utils.jsでは非同期処理を設定していたが、MemoDatabaseからさらに呼び出すようになっていたので簡単化のために処理を統合
    また、値を保持しないクラスは定義の意味がないため削除

--- 04.class/memo-database.js
+++ 04.class/memo-database.js
@@ -1,82 +1,73 @@
 import sqlite3 from "sqlite3";
 
-import DbPromise from "./db_utils.js";
-
 class MemoDatabase {
   constructor() {
     this.db = new sqlite3.Database("./memo.sqlite3");
-    this.promise = new DbPromise();
   }
 
   async ensureTableExists() {
-    try {
-      await this.promise.run(
-        this.db,
-        "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)",
-      );
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        throw err;
-      }
-    }
+    const query =
+      "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)";
+    return new Promise((resolve) => {
+      this.db.run(query, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          resolve(this);
+        }
+      });
+    });
   }
 
   async all() {
-    let memos;
-    try {
-      memos = await this.promise.all(
-        this.db,
-        "SELECT id, text FROM memos ORDER BY id",
-      );
-    } catch (err) {
-      if (err instanceof Error && err.code === "SQLITE_ERROR") {
-        console.error(err.message);
-      } else {
-        throw err;
-      }
-    }
-    return memos;
+    const query = "SELECT id, text FROM memos ORDER BY id";
+    return new Promise((resolve) => {
+      this.db.all(query, (err, rows) => {
+        if (err) {
+          throw err;
+        } else {
+          resolve(rows);
+        }
+      });
+    });
   }
 
   async insert(memo) {
-    try {
-      await this.promise.run(this.db, "INSERT INTO memos (text) VALUES (?)", [
-        memo,
-      ]);
-    } catch (err) {
-      if (err instanceof Error && err.code === "SQLITE_CONSTRAINT") {
-        console.error(err.message);
-      } else {
-        throw err;
-      }
-    }
+    const query = "INSERT INTO memos (text) VALUES (?)";
+    return new Promise((resolve) => {
+      this.db.run(query, memo, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          resolve(this);
+        }
+      });
+    });
   }
 
   async delete(id) {
-    try {
-      await this.promise.run(this.db, "DELETE FROM memos WHERE id = ?", [id]);
-      console.log("Memo deleted.");
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        throw err;
-      }
-    }
+    const query = "DELETE FROM memos WHERE id = ?";
+    return new Promise((resolve, reject) => {
+      this.db.run(query, id, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
   }
 
   async close() {
-    try {
-      await this.promise.close(this.db);
-    } catch (err) {
-      if (err instanceof Error && err.code === "SQLITE_MISUSE") {
-        console.error(err.message);
-      } else {
-        throw err;
-      }
-    }
+    return new Promise((resolve, reject) => {
+      this.db.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 }
 
