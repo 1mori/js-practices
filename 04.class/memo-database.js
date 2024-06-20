8535f7a5fb8a87324fd82cc4ca38942e445ed6fa
yuichi.imori <yuichi.imori@classi.jp>
2024-06-20 13:03:46 +0900

    change: エラー処理を一つにまとめた
    エラー処理が重複していたため、handleErrorにまとめた

--- 04.class/memo-database.js
+++ 04.class/memo-database.js
@@ -9,65 +9,43 @@ class MemoDatabase {
     const query =
       "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)";
     return new Promise((resolve) => {
-      this.db.run(query, (err) => {
-        if (err) {
-          throw err;
-        } else {
-          resolve(this);
-        }
-      });
+      this.db.run(query, (err) => this.#handleError(err, resolve, this));
     });
   }
 
   fetchAll() {
     const query = "SELECT id, text FROM memos ORDER BY id";
     return new Promise((resolve) => {
-      this.db.all(query, (err, rows) => {
-        if (err) {
-          throw err;
-        } else {
-          resolve(rows);
-        }
-      });
+      this.db.all(query, (err, rows) => this.#handleError(err, resolve, rows));
     });
   }
 
   insert(text) {
     const query = "INSERT INTO memos (text) VALUES (?)";
     return new Promise((resolve) => {
-      this.db.run(query, text, (err) => {
-        if (err) {
-          throw err;
-        } else {
-          resolve(this);
-        }
-      });
+      this.db.run(query, text, (err) => this.#handleError(err, resolve, this));
     });
   }
 
   delete(id) {
     const query = "DELETE FROM memos WHERE id = ?";
     return new Promise((resolve) => {
-      this.db.run(query, id, function (err) {
-        if (err) {
-          throw err;
-        } else {
-          resolve(this);
-        }
-      });
+      this.db.run(query, id, (err) => this.#handleError(err, resolve, this));
     });
   }
 
   close() {
     return new Promise((resolve) => {
-      this.db.close((err) => {
-        if (err) {
-          throw err;
-        } else {
-          resolve(this);
-        }
-      });
+      this.db.close((err) => this.#handleError(err, resolve, this));
     });
+  }
+
+  #handleError(err, resolve, result) {
+    if (err) {
+      throw err;
+    } else {
+      resolve(result);
+    }
   }
 }
 
