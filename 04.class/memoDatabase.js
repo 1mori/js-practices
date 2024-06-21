c9753dd5f90d12eafd137e707d7231c115a098d2
yuichi.imori <yuichi.imori@classi.jp>
2024-06-21 12:46:08 +0900

    change: promiseそのものをメソッド化

--- 04.class/memoDatabase.js
+++ 04.class/memoDatabase.js
@@ -8,44 +8,58 @@ class MemoDatabase {
   ensureTableExists() {
     const query =
       "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)";
-    return new Promise((resolve) => {
-      this.db.run(query, (err) => this.#handleError(err, resolve, this));
-    });
+    return this.#runQuery(query);
   }
 
   fetchAll() {
     const query = "SELECT id, text FROM memos ORDER BY id";
-    return new Promise((resolve) => {
-      this.db.all(query, (err, rows) => this.#handleError(err, resolve, rows));
-    });
+    return this.#allQuery(query);
   }
 
   insert(text) {
     const query = "INSERT INTO memos (text) VALUES (?)";
-    return new Promise((resolve) => {
-      this.db.run(query, text, (err) => this.#handleError(err, resolve, this));
-    });
+    return this.#runQuery(query, text);
   }
 
   delete(id) {
     const query = "DELETE FROM memos WHERE id = ?";
-    return new Promise((resolve) => {
-      this.db.run(query, id, (err) => this.#handleError(err, resolve, this));
-    });
+    return this.#runQuery(query, id);
   }
 
   close() {
-    return new Promise((resolve) => {
-      this.db.close((err) => this.#handleError(err, resolve, this));
+    return new Promise((resolve, reject) => {
+      this.db.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
     });
   }
 
-  #handleError(err, resolve, result) {
-    if (err) {
-      throw err;
-    } else {
-      resolve(result);
-    }
+  #runQuery(query, ...params) {
+    return new Promise((resolve, reject) => {
+      this.db.run(query, params, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  #allQuery(query, ...params) {
+    return new Promise((resolve, reject) => {
+      this.db.all(query, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
   }
 }
 
